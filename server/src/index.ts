99e53cbe9887818709ead62b20ad7228613604e0
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-12 10:44:38 +0900

    [UPDATE] 서버 실행부분 수정

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,16 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as helmet from "helmet";
-import * as cors from "cors";
-import api from "./api";
-import logger from "./lib/logger/console"
+import * as http from "http";
+import app from "./app";
+import * as database from "./orm";
 import * as path from "path"
 import * as dotenv from "dotenv"
+import logger from "./lib/logger/console";
 
-dotenv.config({ path: path.join(__dirname, "../.env") });
+dotenv.config({ path: path.join(__dirname, "../.env") })
 
 const { PORT } = process.env;
 
-createConnection()
-  .then(async connection => {
-    const app = express();
-    app.use(cors());
-    app.use(helmet());
-    app.use(bodyParser.json());
-    app.use("/", api)
-    app.listen(PORT || 3000, () => {
-      logger.green(`Server started on port ${PORT}!`);
-    });
-  })
-  .catch(error => logger.red(error));
+database.getConnection();
+
+http.createServer(app).listen(PORT || 8080, () => {
+  logger.green(`Server is listening to ${PORT}`);
+});
