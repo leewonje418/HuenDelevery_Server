b6ca9e86dd28e99575ca374bdeb68f377e51b57d
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-13 11:25:56 +0900

    [UPDATE] 함수 반환형 추가, checkManager, checkDriver 합침, error 처리 수정

---
+++ src/controller/userCtrl.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { validate } from 'class-validator';
+import config from '../config/config';
+import { loginValidate } from '../vaildate/loginValidate'
+import managerLogin from '../service/userService/managerLogin'
+import driverLogin from '../service/userService/driverLogin'
+import logger from '../lib/logger/console';
+
+export class UserCtrl {
+     async checkManager(req: Request, res: Response): Response {
+        const { id, password } = req.body;
+        const LoginValidate: loginValidate = new loginValidate(id, password);
+    
+        const errors = await validate(LoginValidate);
+        if (errors.length > 0) {
+            return res.status(400).json({
+                status: 400,
+                message: '로그인 형식이 맞지 않습니다.'
+            });
+        }
+        try {
+            await managerLogin(id, password);
+            const token = jwt.sign(
+                { userId: id, password: password },
+                config.jwtSecret,
+                { expiresIn: '60h' }
+            );
+            return res.status(200).json({
+                status: 200,
+                message: '로그인 성공~',
+                data: {
+                    'x-access-token' : token,
+                }
+            });
+        } catch (error) {
+            if(error == 'Error: 아이디 비밀번호 맞지 않음!') {
+                return res.status(401).json({
+                    status: 401,
+                    message: '아이디 혹은 비밀번호를 잘못입력하셨습니다.'
+                });
+            } else {
+                logger.error(error);
+                return res.status(500).json({
+                    status: 500,
+                    message: '로그인 오류입니다.'
+                });
+            }
+        }
+    }
+    async checkDriver(req: Request, res: Response): Response {
+        const { id, password } = req.body;
+        const LoginValidate: loginValidate = new loginValidate(id, password);
+    
+        const errors = await validate(LoginValidate);
+        if (errors.length > 0) {
+            return res.status(400).json({
+                status: 400,
+                message: '로그인 형식이 맞지 않습니다.'
+            });
+        }
+        try {
+            await driverLogin(id, password);
+            const token = jwt.sign(
+                { userId: id, password: password },
+                config.jwtSecret,
+                { expiresIn: '60h' }
+            );
+            return res.status(200).json({
+                status: 200,
+                message: '로그인 성공~',
+                data: {
+                    'x-access-token' : token,
+                }
+            });
+        } catch (error) {
+            if(error == 'Error: 아이디 비밀번호 맞지 않음!') {
+                return res.status(401).json({
+                    status: 401,
+                    message: '아이디 혹은 비밀번호를 잘못입력하셨습니다.'
+                });
+            } else {
+                logger.error(error);
+                return res.status(500).json({
+                    status: 500,
+                    message: '로그인 오류입니다.'
+                });
+            }
+        }
+    }
+}
