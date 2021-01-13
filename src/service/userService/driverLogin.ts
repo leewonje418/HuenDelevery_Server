2a21961d33b5e364328a1d57b62a483f307becd5
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-13 11:32:39 +0900

    [UPDATE] ERROR 처리 수정(string으로 리턴하지 않고 throw error 하도록 함)

---
+++ src/service/userService/driverLogin.ts
@@ -0,0 +1,19 @@
+import userRepository from '../../repository/userRepository'
+
+export default async (id: string, password: string) => {
+    let user;
+    try {
+        user = await userRepository.checkDriver(id, password);
+        if(user == undefined) {
+            throw new Error('아이디 비밀번호 맞지 않음!');
+        }
+    } catch (error) {
+        if(error == 'Error: 아이디 비밀번호 맞지 않음!') {
+            throw new Error('아이디 비밀번호 맞지 않음!');
+        } else {
+            console.error(error);
+            throw new Error('서버 오류!');
+        }
+    }
+    return '200';
+}
