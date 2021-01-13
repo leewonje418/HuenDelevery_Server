db62a4043dc9a5fb08acb47b22ee9d48e29b1de4
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-13 12:19:16 +0900

    [UPDATE] driverGetAll 기능 구현

---
+++ src/service/userService/driverGetAll.ts
@@ -0,0 +1,19 @@
+import { Member } from '../../entity/member';
+import { getRepository } from 'typeorm';
+import ERole from'../../enum/eRole'
+
+export default async (): Promise<Member[]> => {
+    const role: ERole = ERole.DRIVER;
+    let returnMember: Member[]
+    try {
+        returnMember = await getRepository(Member).find({
+            where: {
+                role
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        throw new Error('서버 오류!');
+    }
+    return returnMember;
+}
