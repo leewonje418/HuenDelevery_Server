c1813f24e3c577ce5a1d7f3dd79f7e5c5f8829ad
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-14 10:12:59 +0900

    [RENAME] userController -> userRouter

--- src/router/v1/index.ts
+++ src/router/v1/index.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import userContoller from './userRouter'
+import userRouter from './userRouter'
 
 const router: Router = Router();
 
-router.use('/user', userContoller);
+router.use('/user', userRouter);
 
 export default router;
