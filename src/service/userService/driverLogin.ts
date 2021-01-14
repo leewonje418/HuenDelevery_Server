09f25ba3e0e877f123077a9dcd5e5d91e29c9b24
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-14 12:09:43 +0900

    [UPDATE] error처리 수정

--- src/service/userService/driverLogin.ts
+++ src/service/userService/driverLogin.ts
@@ -1,22 +1,20 @@
-import userRepository from '../../repository/userRepository'
+import UserRepository from '../../repository/userRepository'
 import HttpError from '../../error/httpError'
 
 export default async (id: string, password: string) => {
-    let user;
+    const userRepository: UserRepository = new UserRepository();
     try {
-        user = await userRepository.checkDriver(id, password);
-        if(user == undefined) {
+        const user = await userRepository.checkDriver(id, password);
+        if(user === undefined) {
             throw new Error('아이디 비밀번호 맞지 않음!');
         }
     } catch (error) {
-        if(error instanceof HttpError) {
-            if(error.message == 'Error: 아이디 비밀번호 맞지 않음!') {
-                throw new Error('아이디 비밀번호 맞지 않음!');
-            } else {
-                console.error(error);
-                throw new Error('서버 오류!');
-            }
+        const httpError = new HttpError(error);
+        if(httpError.isServerError(httpError.message) == false) {
+            throw new Error('아이디 비밀번호 맞지 않음!');
+        } else {
+            console.error(error);
+            throw new Error('서버 오류!');
         }
     }
-    return;
 }
