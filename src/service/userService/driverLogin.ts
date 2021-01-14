e360bf28cbfb16723574c0f96f390b4f821d2976
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-14 10:13:32 +0900

    [UPDATE] error처리방식 변경

--- src/service/userService/driverLogin.ts
+++ src/service/userService/driverLogin.ts
@@ -1,6 +1,7 @@
 import userRepository from '../../repository/userRepository'
+import HttpError from '../../error/httpError'
 
-export default async (id: string, password: string): Promise<string> => {
+export default async (id: string, password: string) => {
     let user;
     try {
         user = await userRepository.checkDriver(id, password);
@@ -8,12 +9,14 @@ export default async (id: string, password: string): Promise<string> => {
             throw new Error('아이디 비밀번호 맞지 않음!');
         }
     } catch (error) {
-        if(error == 'Error: 아이디 비밀번호 맞지 않음!') {
-            throw new Error('아이디 비밀번호 맞지 않음!');
-        } else {
-            console.error(error);
-            throw new Error('서버 오류!');
+        if(error instanceof HttpError) {
+            if(error.message == 'Error: 아이디 비밀번호 맞지 않음!') {
+                throw new Error('아이디 비밀번호 맞지 않음!');
+            } else {
+                console.error(error);
+                throw new Error('서버 오류!');
+            }
         }
     }
-    return '200';
+    return;
 }
