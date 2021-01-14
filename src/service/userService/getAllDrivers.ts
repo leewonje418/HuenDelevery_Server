88b557778ddc55aca4f6f77ed0c539eaa0d37b8a
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-14 12:10:08 +0900

    [UPDATE] return부분 수정

--- src/service/userService/getAllDrivers.ts
+++ src/service/userService/getAllDrivers.ts
@@ -4,16 +4,15 @@ import Role from'../../enum/Role'
 
 export default async (): Promise<Member[]> => {
     const role: Role = Role.DRIVER;
-    let returnMember: Member[]
     try {
-        returnMember = await getRepository(Member).find({
+        const returnMember: Member[] = await getRepository(Member).find({
             where: {
                 role
             }
         });
+        return returnMember;
     } catch (error) {
         console.error(error);
         throw new Error('서버 오류!');
     }
-    return returnMember;
 }
