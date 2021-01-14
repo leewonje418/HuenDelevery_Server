29aca49007d06f02cf9265c6a06ceab8b4590077
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-14 09:53:34 +0900

    [UPDATE] Error 처리방식 변경, status 삭제

--- src/controller/userCtrl.ts
+++ src/controller/userCtrl.ts
@@ -1,24 +1,23 @@
 import { Request, Response } from 'express';
 import { validate } from 'class-validator';
-import { loginValidate } from '../vaildate/loginValidate'
+import { LoginValidate } from '../vaildate/loginValidate'
 import managerLogin from '../service/userService/managerLogin'
 import driverLogin from '../service/userService/driverLogin'
 import { Member } from '../entity/member';
-import customerGetAll from '../service/userService/customerGetAll'
-import driverGetAll from '../service/userService/driverGetAll'
+import getAllCustomers from '../service/userService/getAllCustomers'
+import getAllDrivers from '../service/userService/getAllDrivers'
 import logger from '../lib/logger/console';
 import { createToken } from'../lib/token';
-import { manager } from'../lib/middleware/auth'
+import HttpError from '../error/httpError'
 
 export class UserCtrl {
      async managerLogin(req: Request, res: Response): Response {
         const { id, password } = req.body;
-        const LoginValidate: loginValidate = new loginValidate(id, password);
+        const loginValidate: LoginValidate = new LoginValidate(id, password);
     
-        const errors = await validate(LoginValidate);
+        const errors = await validate(loginValidate);
         if (errors.length > 0) {
             return res.status(400).json({
-                status: 400,
                 message: '로그인 형식이 맞지 않습니다.'
             });
         }
@@ -26,35 +25,33 @@ export class UserCtrl {
             await managerLogin(id, password);
             const token = await createToken(id);
             return res.status(200).json({
-                status: 200,
                 message: '로그인 성공~',
                 data : {
                     token
                 }
             })
         } catch (error) {
-            if(error == 'Error: 아이디 비밀번호 맞지 않음!') {
-                return res.status(401).json({
-                    status: 401,
-                    message: '아이디 혹은 비밀번호를 잘못입력하셨습니다.'
-                });
-            } else {
-                logger.error(error);
-                return res.status(500).json({
-                    status: 500,
-                    message: '로그인 오류입니다.'
-                });
+            if(error instanceof HttpError) {
+                if(error.message == 'Error: 아이디 비밀번호 맞지 않음!') {
+                    return res.status(401).json({
+                        message: '아이디 혹은 비밀번호를 잘못입력하셨습니다.'
+                    });
+                } else {
+                    logger.error(error.message);
+                    return res.status(500).json({
+                        message: '로그인 오류입니다.'
+                    });
+                }
             }
         }
     }
     async driverLogin(req: Request, res: Response): Response {
         const { id, password } = req.body;
-        const LoginValidate: loginValidate = new loginValidate(id, password);
+        const loginValidate: LoginValidate = new LoginValidate(id, password);
     
-        const errors = await validate(LoginValidate);
+        const errors = await validate(loginValidate);
         if (errors.length > 0) {
             return res.status(400).json({
-                status: 400,
                 message: '로그인 형식이 맞지 않습니다.'
             });
         }
@@ -62,61 +59,48 @@ export class UserCtrl {
             await driverLogin(id, password);
             const token = await createToken(id);
             return res.status(200).json({
-                status: 200,
                 message: '로그인 성공~',
                 data: {
                     'x-access-token' : token,
                 }
             });
         } catch (error) {
-            if(error == 'Error: 아이디 비밀번호 맞지 않음!') {
-                return res.status(401).json({
-                    status: 401,
-                    message: '아이디 혹은 비밀번호를 잘못입력하셨습니다.'
-                });
-            } else {
-                logger.error(error);
-                return res.status(500).json({
-                    status: 500,
-                    message: '로그인 오류입니다.'
-                });
+            if(error instanceof HttpError) {
+                if(error.message == 'Error: 아이디 비밀번호 맞지 않음!') {
+                    return res.status(401).json({
+                        message: '아이디 혹은 비밀번호를 잘못입력하셨습니다.'
+                    });
+                } else {
+                    logger.error(error.message);
+                    return res.status(500).json({
+                        message: '로그인 오류입니다.'
+                    });
+                }
             }
         }
     }
-    async customerGetAll(req: Request, res: Response): Response {
+    async getAllCustomers(req: Request, res: Response): Response {
         try {
-            const data: Member[] = await customerGetAll();
-            const managerCheck = await manager(req, res);
-            if(managerCheck !== 'success') {
-                return managerCheck;
-            }
+            const data: Member[] = await getAllCustomers();
             return res.status(200).json({
-                status: 200,
                 message: '고객 조회 성공~',
                 data
             });
         } catch (error) {
             return res.status(500).json({
-                status: 500,
                 message: '고객 조회 오류입니다.'
             });
         }
     }
-    async driverGetAll(req: Request, res: Response): Response {
+    async getAllDrivers(req: Request, res: Response): Response {
         try {
-            const data: Member[] = await driverGetAll();
-            const managerCheck = await manager(req, res);
-            if(managerCheck !== 'success') {
-                return managerCheck;
-            }
+            const data: Member[] = await getAllDrivers();
             return res.status(200).json({
-                status: 200,
                 message: '드라이버 조회 성공~',
                 data
             });
         } catch (error) {
             return res.status(500).json({
-                status: 500,
                 message: '드라이버 조회 오류입니다.'
             });
         }
