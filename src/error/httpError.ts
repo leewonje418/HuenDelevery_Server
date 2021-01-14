54382f4efb6ca2b0c08d29d8fda11cdd6a6ba674
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-14 12:07:36 +0900

    [UPDATE] default 속성 제거

--- src/error/httpError.ts
+++ src/error/httpError.ts
@@ -1,5 +1,14 @@
-export default class HTTPError extends Error {
-    constructor(message?: string) {
+export class HttpError extends Error {
+    constructor(message: string) {
         super(message);
+    }
+    isServerError(message: string): boolean {
+        let serverError: boolean = false;
+        if(message == 'Error: 아이디 비밀번호 맞지 않음!') {
+            return serverError;
+        } else {
+            serverError = true;
+            return serverError;
+        }
     }
 }
