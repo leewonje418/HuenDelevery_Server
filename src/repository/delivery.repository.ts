0b13129053b83e180cfd93c7a16d28fc5f19e45f
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-18 11:03:19 +0900

    [UPDATE] deliveryrepository

--- src/repository/delivery.repository.ts
+++ src/repository/delivery.repository.ts
@@ -3,4 +3,12 @@ import { Delivery } from '../entity/delivery';
 
 @EntityRepository(Delivery)
 export default class DeliveryRepository extends Repository<Delivery> {
+    // findByEndtimeAnd = async (id: string, password: string): Promise<Delivery[] | undefined> => {
+    //     return this.find({
+    //         where: {
+    //             id,
+    //             password,
+    //         },
+    //     });
+    // }
 }
