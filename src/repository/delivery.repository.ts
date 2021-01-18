e2e2b2716e2698322849b533afea03fcf0fff1e7
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-01-18 11:56:20 +0900

    Fix: 완료된 Delivery 응답 시 Driver, Customer join

--- src/repository/delivery.repository.ts
+++ src/repository/delivery.repository.ts
@@ -3,8 +3,10 @@ import { Delivery } from '../entity/delivery';
 
 @EntityRepository(Delivery)
 export default class DeliveryRepository extends Repository<Delivery> {
-    findByEndTime = async (date: string) => {
-        return this.createQueryBuilder()
+    findByEndTime = async (date: string): Promise<Delivery[]> => {
+        return this.createQueryBuilder('delivery')
+            .leftJoinAndSelect('delivery.driver', 'driver')
+            .leftJoinAndSelect('delivery.customer', 'customer')
             .where('DATE(wr_5) = :date', { date })
             .orderBy('wr_5', 'DESC')
             .getMany();
