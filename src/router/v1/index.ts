0cd008e3a736136bae2d68efd9145cb810e241db
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-01-18 09:54:14 +0900

    [UPDATE] delivery url 추가

--- src/router/v1/index.ts
+++ src/router/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRouter from './userRouter'
 import authRouter from './authRouter'
+import deliveryRouter from './deliveryRouter'
 
 const router: Router = Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/delivery', deliveryRouter);
 
 export default router;
