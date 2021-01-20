85a4c4c76abc18c96c018787c2e85c06730af1e1
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-01-20 13:41:50 +0900

    Add: Address를 위도, 경도로 변환하는 함수 제작

--- src/thirdParty/google.ts
+++ src/thirdParty/google.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { GOOGLE } from '../../config/config';
 import HttpError from '../error/httpError';
+import { ILatLong } from '../interface/position.interface';
 
 const GOOGLE_END_POINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-export const convertToAddress = async (lat: number, long: number) => {
+export const convertToAddress = async (lat: number, long: number): string => {
   const res = await axios.get(GOOGLE_END_POINT, {
     params: {
       latlng: encodeURI(`${lat},${long}`),
@@ -17,5 +18,25 @@ export const convertToAddress = async (lat: number, long: number) => {
     return res.data['results'][0]['formatted_address'];
   } catch (err) {
     return `위도: ${lat}, 경도: ${long}`;
+  }
+}
+
+export const convertToPosition = async (address: string): Promise<ILatLong> => {
+  const res = await axios.get(GOOGLE_END_POINT, {
+    params: {
+      key: GOOGLE.KEY,
+      address,
+    },
+  });
+
+  try {
+    const result = res.data['results'][0]['geometry']['location'];
+
+    return {
+      lat: result['lat'],
+      long: result['lng'],
+    };
+  } catch (err) {
+    throw new HttpError(400, '알 수 없는 주소');
   }
 }
