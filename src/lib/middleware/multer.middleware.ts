8edc3e38b58b76d2ce22bebc32de2f892dba2953
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-01-20 21:25:59 +0900

    Fix: file 저장 이름 변경

--- src/lib/middleware/multer.middleware.ts
+++ src/lib/middleware/multer.middleware.ts
@@ -8,7 +8,16 @@ const upload = multer({
       cb(null, path.join(__dirname, `../../../public`));
     },
     filename: (_req, file, cb) => {
-      cb(null, `${Date.now()}_${file.originalname}`);
+      const uuid: string = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        // tslint:disable-next-line: no-bitwise
+        const r = Math.random() * 16 | 0;
+        // tslint:disable-next-line: no-bitwise
+        const v = c === 'x' ? r : (r & 3 | 8);
+        return v.toString();
+      });
+
+      const extName = path.extname(file.originalname)
+      cb(null, `${Date.now()}_${uuid}${extName}`);
     },
   })
 }).single('file');
@@ -24,3 +33,4 @@ export default (req: Request, res: Response, next: NextFunction) => {
     }
   });
 };
+
